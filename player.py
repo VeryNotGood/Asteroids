77fe268cd92825e6549e21a6ca6a43472b8fc2bc
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-12 21:13:50 -0600

    cleaned up imports, created self.vertices for TriangleShape, Player is now TriangleShape

--- player.py
+++ player.py
@@ -1,25 +1,28 @@
-from circleshape import *
-from constants import *
-from shot import *
+import pygame
+from shapes import TriangleShape
+from constants import PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_TURN_SPEED, PLAYER_RADIUS
+from shot import Shot
 
-class Player(CircleShape):
+
+class Player(TriangleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
         self.x = x
         self.y = y
         self.cooldown = 0
-    # in the player class
+        self.rotation = 0
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
-        return [a, b, c]
-    
+        self.vertices = [a, b, c]
+        return self.vertices
+
     def draw(self, screen):
-        pygame.draw.polygon(screen, 'white', self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -40,12 +43,12 @@ class Player(CircleShape):
             if self.cooldown <= 0:
                 self.shoot()
                 self.cooldown = 0.3
-            
+
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
         shot = Shot(self.position.x, self.position.y)
-        shot_forward = pygame.Vector2(0,1).rotate(self.rotation)
+        shot_forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity += shot_forward * PLAYER_SHOOT_SPEED
